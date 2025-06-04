64331805e25e6892d0a5e19fffec1daf519fa3e5
hyerin <rin4219@naver.com>
2025-06-04 17:41:35 +0900

    week10

---
+++ src/main/java/com/umc/demo/config/security/CustomUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.umc.demo.config.security;
+
+import com.umc.demo.domain.User;
+import com.umc.demo.repository.UserRepository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("해당 이메일을 가진 유저가 존재하지 않습니다: " + email));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getEmail())
+                .password(user.getPassword())
+                .roles(user.getRole().name())
+                .build();
+    }
+}
